c39d69d8794a4570ad8e646843b9ba3584f0d967
Ben John <github@benjohn.de>
2018-10-05 09:22:26 +0200

    fix: refactored ZXImage

--- Sources/Core/ZXImage.swift
+++ Sources/Core/ZXImage.swift
@@ -6,7 +6,7 @@
 // - Date: 04.10.18
 //
 // 
-    
+
 
 import Foundation
 import CoreGraphics
@@ -14,71 +14,73 @@ import CoreImage
 import QuartzCore
 import ImageIO
 
-#if TARGET_OS_EMBEDDED || TARGET_IPHONE_SIMULATOR
-#endif
-
-class ZXImage: NSObject {
+class ZXImage {
     private(set) var cgimage: CGImage
     
-    convenience init(matrix: ZXBitMatrix) {
+    convenience init(matrix: ZXBitMatrix) throws {
         let colorSpace = CGColorSpaceCreateDeviceGray()
+
+        let black = CGColor(colorSpace: colorSpace, components: [0.0, 1.0])!
+        let white = CGColor(colorSpace: colorSpace, components: [1.0, 1.0])!
         
-        let blackComponents = [0.0, 1.0]
-        let black = CGColor(colorSpace: colorSpace, components: blackComponents)
-        let whiteComponents = [1.0, 1.0]
-        let white = CGColor(colorSpace: colorSpace, components: whiteComponents)
-        
-        let result = self.init(matrix: matrix, on: black, offColor: white)
-        
-        CGColorRelease(white)
-        CGColorRelease(black)
+        try self.init(matrix: matrix, on: black, offColor: white)
     }
     
-    convenience init(matrix: ZXBitMatrix, on onColor: CGColor, offColor: CGColor) {
-        let onIntensities = [UInt8](repeating: 0, count: 4)
-        let offIntensities = [UInt8](repeating: 0, count: 4)
+    init(matrix: ZXBitMatrix, on onColor: CGColor, offColor: CGColor) throws {
+        var onIntensities = [UInt8](repeating: 0, count: 4)
+        var offIntensities = [UInt8](repeating: 0, count: 4)
         
-        self.setColorIntensities(&onIntensities, color: onColor)
-        self.setColorIntensities(&offIntensities, color: offColor)
+        ZXImage.setColorIntensities(intensities: &onIntensities, color: onColor)
+        ZXImage.setColorIntensities(intensities: &offIntensities, color: offColor)
         
-        let width = matrix?.width
-        let height = matrix?.height
-        let bytes = Int8(malloc(width * height * 4))
-        for y in 0..<(height ?? 0) {
-            for x in 0..<(width ?? 0) {
-                let bit = matrix?.getX(x, y: y)
+        let width = matrix.width
+        let height = matrix.height
+        var bytes = [UInt8](repeating: 0, count: width * height * 4)
+        for y in 0 ..< height {
+            for x in 0 ..< width {
+                let bit = matrix.getX(x, y: y)
                 for i in 0..<4 {
-                    let intensity = bit ?? false ? onIntensities[i] : offIntensities[i]
-                    bytes[y * (width ?? 0) * 4 + x * 4 + i] = intensity
+                    let intensity = bit ? onIntensities[i] : offIntensities[i]
+                    bytes[y * width * 4 + x * 4 + i] = intensity
                 }
             }
         }
-        
+
         let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let c = CGContext(data: bytes, width: width, height: height, bitsPerComponent: 8, bytesPerRow: 4 * (width ?? 0), space: colorSpace, bitmapInfo: CGBitmapInfo.alphaInfoMask.rawValue & CGImageAlphaInfo.premultipliedLast.rawValue)
-        let image = c.makeImage()
-        free(bytes)
-        
-        let zxImage = self.init(cgImageRef: image)
-    }
-    
-    init(cgImageRef image: CGImage) {
-        
-    }
-    
-    init(url: URL) {
-        let provider = CGDataProviderCreateWithURL(url as? CFURL?)
-        
-        if provider != nil {
-            let source = CGImageSourceCreateWithDataProvider(provider, 0)
-            
-            if source != nil {
-                cgimage = CGImageSourceCreateImageAtIndex(source, 0, 0)
-            }
-            
-            CGDataProviderRelease(provider)
+        let c = CGContext(
+            data: UnsafeMutablePointer(mutating: bytes),
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
+            bytesPerRow: 4 * width,
+            space: colorSpace,
+            bitmapInfo: CGBitmapInfo.alphaInfoMask.rawValue & CGImageAlphaInfo.premultipliedLast.rawValue
+        )
+        guard let image = c?.makeImage() else {
+            // TODO
+            throw ZXError.internalInconsistencyException("")
         }
-        
+        cgimage = image
+    }
+    
+    init(cgImage image: CGImage) {
+        self.cgimage = image
+    }
+    
+    init(url: URL) throws {
+        guard let provider = CGDataProvider(url: url as CFURL) else {
+            // TODO
+            throw ZXError.internalInconsistencyException("")
+        }
+        guard let source = CGImageSourceCreateWithDataProvider(provider, [:] as CFDictionary) else {
+            // TODO
+            throw ZXError.internalInconsistencyException("")
+        }
+        guard let cgimage = CGImageSourceCreateImageAtIndex(source, 0, [:] as CFDictionary) else {
+            // TODO
+            throw ZXError.internalInconsistencyException("")
+        }
+        self.cgimage = cgimage
     }
     
     var width: Int {
@@ -89,21 +91,22 @@ class ZXImage: NSObject {
         return cgimage.height
     }
     
-    class func setColorIntensities(_ intensities: UnsafeMutablePointer<UInt8>?, color: CGColor?) {
-        memset(intensities, 0, 4)
-        
-        let numberOfComponents = color?.numberOfComponents
-        let components = color?.components
-        
+    class func setColorIntensities(intensities: inout [UInt8], color: CGColor) {
+        // memset(intensities, 0, 4)
+        intensities.insert(contentsOf: [0, 0, 0, 0], at: 0)
+
+        let numberOfComponents = color.numberOfComponents
+        let components = color.components!
+
         if numberOfComponents == 4 {
             for i in 0..<4 {
-                intensities?[i] = min(1.0, max(0, components?[i])) * 255
+                intensities[i] = UInt8(min(1.0, max(0, components[i])) * 255)
             }
         } else if numberOfComponents == 2 {
             for i in 0..<3 {
-                intensities?[i] = min(1.0, max(0, components?[0])) * 255
+                intensities[i] = UInt8(min(1.0, max(0, components[0])) * 255)
             }
-            intensities?[3] = min(1.0, max(0, components?[1])) * 255
+            intensities[3] = UInt8(min(1.0, max(0, components[1])) * 255)
         }
     }
 }
